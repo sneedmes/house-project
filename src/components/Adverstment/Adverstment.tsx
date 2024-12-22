d364ebd66376bd0588df75644966b7bd766ec419
sneedmes <dallaqyan0610@gmail.com>
2024-12-23 00:35:30 +0400

    feat: changed styles for promo section

--- src/components/Adverstment/Adverstment.tsx
+++ src/components/Adverstment/Adverstment.tsx
@@ -8,18 +8,23 @@ export const Adverstment=()=>{
         <>
             <Container disableGutters className={`${styles.ad}`}>
 
-                <Box component="section" className={`${styles.ad_title}`}>
+                <Box component="section" className={`${styles.ad_container}`}>
+                    <Box component="section" className={`${styles.ad_title}`}>
                     <img src="logo-02.png" alt="icon"/>
                     <h2>Новая коллекция французских авторов</h2>
-                </Box>
+                    </Box>
 
-                <Box component="section" className={`${styles.ad_text}`}>
-                    <p>Сложно сказать, почему акционеры крупнейших компаний призывают нас к новым свершениям, которые, в свою очередь, должны быть заблокированы в рамках своих собственных рациональных ограничений.</p>
-                    <p>Принимая во внимание показатели успешности, граница обучения кадров предопределяет высокую востребованность направлений прогрессивного развития.</p>
-                </Box>
+                    <Box component="section" className={`${styles.ad_text}`}>
+                        <p>Сложно сказать, почему акционеры крупнейших компаний призывают нас к новым свершениям,
+                            которые, в свою очередь, должны быть заблокированы в рамках своих собственных рациональных
+                            ограничений.</p>
+                        <p>Принимая во внимание показатели успешности, граница обучения кадров предопределяет высокую
+                            востребованность направлений прогрессивного развития.</p>
+                    </Box>
 
-                <Box component="section" className={`${styles.ad_button}`}>
-                    <Button position="ad" title="Ознакомиться" onClick={()=>null} isActive={false}/>
+                    <Box component="section" className={`${styles.ad_button}`}>
+                        <Button position="ad" title="Ознакомиться" onClick={() => null} isActive={false}/>
+                    </Box>
                 </Box>
 
             </Container>
